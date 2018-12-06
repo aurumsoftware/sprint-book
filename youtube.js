00ff94c008e06b139c15bf3f240d1d3a808c42bb
Eduardo Wichoski <eduardowichoski@MacBook-Aurum.local>
2018-12-06 16:07:00 -0200

    ajuste

--- youtube.js
+++ youtube.js
@@ -4,7 +4,19 @@ $( document ).ready(function() {
         if(textos.length > 0){
             for (let index = 0; index < textos.length; index++) {
                 if($($(textos[index])[0]).text().startsWith("https://www.youtube.com")){
-                    $($(textos[index])[0]).html('<iframe width="250"  src="'+ $($(textos[index])[0]).text() +'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+                    $($(textos[index])[0]).html('<iframe width="500"  height="281" src="'+ $($(textos[index])[0]).text() +'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+                }   
+            }
+        }       
+    });
+
+
+    $("#chatlioWidgetPlaceholder").bind("DOMNodeInserted",function(){
+        var textos = $(this).find("span.chatlio-message-body-text");
+        if(textos.length > 0){
+            for (let index = 0; index < textos.length; index++) {
+                if($($(textos[index])[0]).text().startsWith("datepicker")){
+                    $($(textos[index])[0]).html('insere um datepicker aqui');
                 }   
             }
         }       

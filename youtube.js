dc22ce83643b42a205208f2aeadac586b8e27eaa
Gustavo Maestri <gumaestri@gmail.com>
2018-12-06 15:20:44 -0200

    add chatbot

---
+++ youtube.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+    $("#chatlioWidgetPlaceholder").bind("DOMNodeInserted",function(){
+        var textos = $(this).find("span.chatlio-message-body-text");
+        if(textos.length > 0){
+            for (let index = 0; index < textos.length; index++) {
+                if($($(textos[index])[0]).text().startsWith("https://www.youtube.com")){
+                    $($(textos[index])[0]).html('<iframe width="250"  src="'+ $($(textos[index])[0]).text() +'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+                }
+                
+                
+            }
+        }
+        
+        
+    });
+});
